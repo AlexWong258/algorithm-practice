f33e721985532c600fe8f332fb0e0468a98053b6
AlexWong258 <1193901615@qq.com>
2019-10-16 10:33:12 +0800

    quicksort

---
+++ src/quicksort/QuickSort2.java
@@ -0,0 +1,45 @@
+package quicksort;
+
+public class QuickSort2 {
+    public static void qSort(int[] num, int low, int high) {
+        if (low < high) {
+            int pivot = partition(num, low, high);
+
+            qSort(num, low, pivot - 1);
+            qSort(num, pivot + 1, high);
+        }
+    }
+
+    private static int partition(int[] num, int low, int high) {
+        int pivotKey = num[low];
+        int initLow = low;
+
+        while (low < high) {
+            while (low < high && num[high] >= pivotKey) high--;
+            while (low < high && num[low] <= pivotKey) low++;
+
+            if (low < high) {
+                swap(num, low, high);
+            }
+        }
+
+        swap(num, initLow, low);
+
+        return low;
+    }
+
+    private static void swap(int[] num, int low, int high) {
+        int temp = num[low];
+        num[low] = num[high];
+        num[high] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] num = {8, 7, 4, 3, 9, 7, 2, 6, 1, 5, 7};
+        qSort(num, 0, num.length - 1);
+
+        for (int i = 0; i < num.length; i++){
+            System.out.println(num[i]);
+        }
+    }
+}
